433ec6ed7c7c2c096d6c4b487d3276f18f543937
thedemonon <zamora88875@gmail.com>
2026-03-05 14:27:29 -0500

    Changed grid repeat function to use original auto-fit

--- svelte/src/routes/meditations/+page.svelte
+++ svelte/src/routes/meditations/+page.svelte
@@ -75,14 +75,14 @@
 
   .meditations {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(384px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(384px, 1fr));
     gap: 20px;
     justify-items: center;
   }
 
   @media (max-width: 500px) {
     .meditations {
-      grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     }
   }
 </style>
